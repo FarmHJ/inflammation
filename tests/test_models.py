51f3eda89ed8695572fd07026c73a3795f7f69e3
FarmHJ <huijia-farm_95@hotmail.com>
2020-10-19 10:53:45 +0100

    Add initial test cases for daily_max() and daily_min()

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import numpy as np
 import numpy.testing as npt
 from unittest.mock import patch
+import pytest
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
@@ -29,6 +30,70 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(np.array([3, 4]), daily_mean(test_array))
 
+def test_daily_max():
+    """Test that max function works for an array of positive integers."""
+    from inflammation.models import daily_max
+
+    test_array = np.array([[4, 2, 5],
+                           [1, 6, 2],
+                           [4, 1, 9]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([4, 6, 9]), daily_max(test_array))
+
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers."""
+    from inflammation.models import daily_min
+
+    test_array = np.array([[4, -2, 5],
+                           [1, -6, 2],
+                           [-4, -1, 9]])  # yapf: disable
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(np.array([-4, -6, 2]), daily_min(test_array))
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+     ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+     ([[1, 2], [3, 4], [5, 6]], [3, 4])
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    from inflammation.models import daily_mean
+    npt.assert_array_equal(np.array(expected), daily_mean(np.array(test)))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+     ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+     ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [4, 6, 9]),
+     ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [4, -1, 9])
+    ])
+def test_daily_mean(test, expected):
+    """Test max function works for array of zeroes, positive integers, mix of positive/negative integers."""
+    from inflammation.models import daily_max
+    npt.assert_array_equal(np.array(expected), daily_max(np.array(test)))
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+     ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+     ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [1, 1, 2]),
+     ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [-4, -6, 2])
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes, positive integers, mix of positive/negative integers."""
+    from inflammation.models import daily_min
+    npt.assert_array_equal(np.array(expected), daily_min(np.array(test)))  
+
 @patch('inflammation.models.get_data_dir', return_value='/data_dir')
 def test_load_csv(mock_get_data_dir):
     from inflammation.models import load_csv
@@ -42,3 +107,5 @@ def test_load_csv(mock_get_data_dir):
 
 # TODO(lesson-automatic) Implement tests for the other statistical functions
 # TODO(lesson-mocking) Implement a unit test for the load_csv function
+
+ 
